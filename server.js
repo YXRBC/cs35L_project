3aebdb10140aecd323276c06f0df7904e057200e
jimmyzhenyaosun <68259190+jimmyzhenyaosun@users.noreply.github.com>
2021-05-16 13:19:59 +0800

    add error message

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const loginRouter= require('./routes/index')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');const express = require('express')
+
+const indexRouter= require('./routes/index')
 const commentRouter = require('./routes/comment')
 const app = express()
 
@@ -7,10 +13,26 @@ app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({extended: false}))
 app.use('/comment', commentRouter)
-app.use('/',loginRouter)
+app.use('/',indexRouter)
 
 app.get('/', (req, res)=> {
     res.send('waiting for homepage')
 })
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(3000)
