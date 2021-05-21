cef051ea0a8e838d9a05fb32e0f1add2e2b81d7e
Yuetong Xu <rebeccaxuyy12@g.ucla.edu>
2021-05-20 19:51:53 -0700

    use database for accessing class info and changing class info; add class to database

--- routes/comment.js
+++ routes/comment.js
@@ -1,6 +1,33 @@
 const { request } = require('express')
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const {url} = require('../db.js')
+
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true, 
+    useFindAndModify: false
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+})
+
+var classSchema = mongoose.Schema({
+    name: String,
+    info: String,
+    professors:String,
+    summary:String,
+    overall_rating: Number,
+    num_rating:Number
+})
+var Class = mongoose.model("class",classSchema)
+
 var comment = [{
     user: 'username',
     commentAt: new Date(),
@@ -23,23 +50,39 @@ router.get('/rate', (req, res) => {
     res.render('comment/rate')
 } )
 router.get('/classpage',(req,res)=>{
-    res.render('comment/index', {comment: comment, class_id: class_id[0]})  
+    //req be class name ideal
+    var display_class
+    Class.find({name: 'TEST1'},function(err,response){
+        if (err){
+            console.log("can't find class")
+            throw err
+        }
+        display_class = response[0]
+        res.render('comment/index', {comment: comment, class_id: display_class})  
+    })
+    //res.render('comment/index', {comment: comment, class_id: display_class})  
 })
 router.get('/add_class',(req,res)=>{
     res.render('comment/create')
 })
 
 router.post('/addition',(req,res)=>{
-    const new_class = {
+    var new_class = new Class({
         name: req.body.class_name,
         info: req.body.info,
         professors: req.body.professor,
         summary:req.body.summary,
         overall_rating: 0,
         num_rating: 0
-    }
-    class_id.push(new_class)
-    console.log(class_id)
+    })
+    new_class.save(function(err,Class){
+        if(err){
+            console.log("database error: fail save new class")
+        }
+        else{
+            console.log("new class added to database")
+        }
+    })
     res.redirect('/comment/classpage')
 
 })
@@ -51,18 +94,26 @@ router.post('/useful', (req,res) =>{
 })
 
 router.post('/rate', (req,res)=>{
-    let num = req.body.rate *1
-    if(class_id.overall_rating === null){
-        class_id.overall_rating = num
-        class_id.num_rating = 1
-    }
-    else{
-        let total = (class_id.overall_rating * class_id.num_rating) + num
-        let rate_num = class_id.num_rating +1
-        class_id.overall_rating = total/rate_num
-        class_id.num_rating ++
-    }
-    res.redirect('/comment/classpage')
+    var display_class
+    var class_name = req.body.class
+    Class.find({name: class_name},function(err,response){
+        if (err){
+            console.log("can't find class")
+            throw err
+        }
+        display_class = response[0]
+        let num = req.body.rate *1
+        let total = (display_class.overall_rating * display_class.num_rating) + num
+        let rate_num = display_class.num_rating +1
+        Class.findOneAndUpdate({name: class_name}, {overall_rating: total/rate_num, num_rating:rate_num}, function(err, response) {
+            if(err){
+                console.log("unsuccessful update of rating")
+                throw err
+            }
+         })
+         res.redirect('/comment/classpage')
+    })
+    //res.redirect('/comment/classpage')
 })
 
 router.post('/', (req,res) =>{
