04f9395afa8b082f5ac534e299c56d9ea531da86
Yuetong Xu <rebeccaxuyy12@g.ucla.edu>
2021-05-23 10:47:35 -0700

    classes of same name can be only added once to database;add description to code

--- routes/comment.js
+++ routes/comment.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const {classSchema} = require('./schema.js')
 const {url} = require('../db.js')
 
+//connect to database
 const connectionParams={
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -27,27 +28,22 @@ var comment = [{
     courseComment: 'content of comment',
     usefulness:0
   }]
-var class_id = [{
-    name: 'class_name',
-    info: 'basic_info',
-    professors: 'prof_name',
-    summary:'course_content_summary',
-    overall_rating: 0,
-    num_rating: 0
-}]
 
+//create new comment
 router.get('/new', (req, res) => {
     res.render('comment/new')
 } )
 router.get('/rate', (req, res) => {
     res.render('comment/rate')
 } )
+
+//display classpage
 router.get('/classpage',(req,res)=>{
     //req be class name ideal
     var display_class
     Class.find({name: 'TEST1'},function(err,response){
         if (err){
-            console.log("can't find class")
+            console.log("error in finding class")
             throw err
         }
         display_class = response[0]
@@ -59,6 +55,8 @@ router.get('/add_class',(req,res)=>{
     res.render('comment/create')
 })
 
+//add a new class to the database
+//add success only if the class isn't already in the database
 router.post('/addition',(req,res)=>{
     var new_class = new Class({
         name: req.body.class_name,
@@ -68,26 +66,37 @@ router.post('/addition',(req,res)=>{
         overall_rating: 0,
         num_rating: 0
     })
-    new_class.save(function(err,Class){
-        if(err){
-            console.log("database error: fail save new class")
+    Class.find({name: req.body.class_name},function(err,response){
+        if (err){
+            console.log("error in finding class")
+            throw err
+        }
+        if(response.length > 0){
+            console.log("class already exist in database")
         }
         else{
-            console.log("new class added to database")
+            new_class.save(function(err,Class){
+                if(err){
+                    console.log("database error: fail save new class")
+                }
+                else{
+                    console.log("new class added to database")
+                }
+            })
         }
     })
     res.redirect('/comment/classpage')
-
 })
 
 
-
+//update comment usefulness count
 router.post('/useful', (req,res) =>{
     let num = req.body.com
     comment[num].usefulness ++
     res.redirect('/comment/classpage')
 })
 
+//update overall rating of the class
 router.post('/rate', (req,res)=>{
     var display_class
     var class_name = req.body.class
@@ -111,6 +120,7 @@ router.post('/rate', (req,res)=>{
     //res.redirect('/comment/classpage')
 })
 
+//save new comment to database and display in classpage
 router.post('/', (req,res) =>{
     const new_comment = {
         user: req.body.username,
